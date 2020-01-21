3d9d1d9fe7aa23d3a441b5a81378ad95d2d5c2be
System Administrator <root@alexanders-mbp.localdomain>
2020-01-21 11:25:07 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "notes-app-uploads-alllex229"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://ac5o1eqxwg.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_EmGY1RqWl",
+      APP_CLIENT_ID: "2in9f8oj9uvpr3evshuvj04r60",
+      IDENTITY_POOL_ID: "us-east-1:7d47246f-57a4-4603-a499-539c945672bf"
+    }
+  };
